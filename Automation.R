8621b153d89c98b9510c7634743e3da929ac2081
Justin Feathers <jzfeathe@ncsu.edu>
2022-11-14 12:44:08 -0500

    testing automation with .md file extension

--- Automation.R
+++ Automation.R
@@ -1,10 +1,8 @@
 library(rmarkdown)
-
 channelIDs <- list("lifestyle", "entertainment", "bus", "socmed", "tech", "world")
-output_file <- paste0(channelIDs, ".html")
+output_file <- paste0(channelIDs, ".md")
 parameters = lapply(channelIDs, FUN = function(x){list(channel = x)})
 reports <- tibble(output_file, parameters)
-
 apply(reports, MARGIN = 1,
       FUN = function(x){
         render(input = "./project-3.Rmd", output_file = x[[1]], params = x[[2]])
